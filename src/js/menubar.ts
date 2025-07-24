fe70cc14fe63b9026d088aa4cd87aa0be4faac08
Alexander Olsen <aolsen@ucsd.edu>
2025-07-23 19:17:23 -0700

    format menu tabs, refactor recenter button, remove contributors window

--- src/js/menubar.ts
+++ src/js/menubar.ts
@@ -5,3 +5,14 @@ function handleClick(name) {
 function handleToggle(checkbox, label) {
   console.log(`${label} is now ${checkbox.checked ? "enabled" : "disabled"}`);
 }
+
+
+// initialize menu stuff
+import { reCenterAllWindows } from "./panels";
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById("recenter-windows")?.addEventListener("click", () => { 
+    reCenterAllWindows();
+  });
+})
+
