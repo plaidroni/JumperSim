8c3497ed2ba5b796047736502cd5532143108cd0
Alexander Olsen <aolsen@ucsd.edu>
2025-08-11 20:58:36 -0700

    refactor dragging into new Panel component

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import '../css/devconsole.css'
 
 interface PanelProps {
@@ -12,16 +12,41 @@ interface PanelProps {
 
 // children accessed through,,, children
 export default function Panel(props: PanelProps) {
-    const position = useRef(0);
+    const [isDragging, setIsDragging] = useState(false);
+    let dragOffset = useRef({x: 0, y: 0});
+    let [pos, setPos] = useState({x: 100, y: 100});
+
+    function handleMouseDown(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        // get the offset from the top corner
+        dragOffset.current.x = event.clientX - pos.x
+        dragOffset.current.y = event.clientY - pos.y
+        setIsDragging(true);        
+    }
+
+    function handleMouseMove(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        if (isDragging) {
+            let newPos = {x: 0, y: 0};
+            newPos.x = event.clientX - dragOffset.current.x;
+            newPos.y = event.clientY - dragOffset.current.y;
+            setPos(newPos); // i think there's a cleaner looking way to do this
+        }
+    }
+
+    function handleMouseUp(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        if (isDragging) {
+            setIsDragging(false);
+        }
+    }
 
     return ( 
-        <div className="panel">
+        <div className="panel" style={{ position: 'absolute', left: pos.x, top: pos.y }}>
             <div 
                 className="panel-header" 
-                draggable="true"
-                onDrag={handleDrag}
+                onMouseDown={e => handleMouseDown(e)}
+                onMouseMove={e => handleMouseMove(e)}
+                onMouseUp={e => handleMouseUp(e)}
             >
-                <h2>{props.title || 'Untitled'}</h2>
+                <h2>{props.title}</h2>
                 <div className="panel-controls">
                     {/* TODO: Add onclick actions for panels */}
                     <button onClick={props.minimize}>-</button>
@@ -34,9 +59,3 @@ export default function Panel(props: PanelProps) {
         </div>
     );
 }
-
-// update the panel position when the header is dragged
-function handleDrag(e) {
-
-}
-
