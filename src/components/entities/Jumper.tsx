aedb340a15a81dfd1c36f22f66685360b6dbe13e
Alexander Olsen <aolsen@ucsd.edu>
2025-08-17 17:01:46 -0700

    create initial jumper file

---
+++ src/components/entities/Jumper.tsx
@@ -0,0 +1,17 @@
+import { SimJumper } from "../../js/classes/SimEntities";
+import * as BaseEntities from "../../js/classes/BaseEntities";
+
+interface JumperProps {
+    SuitType?: BaseEntities.SuitType;
+    FlyingType?: BaseEntities.FlyingStyle;
+}
+
+function Jumper(props: JumperProps) {
+    return ( 
+        <mesh>
+
+        </mesh>
+     );
+}
+
+export default Jumper;
